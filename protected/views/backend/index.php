168be91774653cc767b864704c6b760afb4f6dc4
kimniyom <kimniyomclub@gmail.com>
2016-11-12 16:25:09 +0700

    new commit

--- protected/views/backend/index.php
+++ protected/views/backend/index.php
@@ -3,6 +3,9 @@
         height: 50px;
         font-size: 30px;
     }
+    #bg-patient{
+        background: url("images/bg-patient.png") no-repeat right bottom fixed;
+    }
 </style>
 <script>
     $(document).ready(function () {
@@ -11,44 +14,45 @@
 </script>
 
 <?php $config = new Configweb_model(); ?>
-<br/><br/>
 
-<div class="row">
-    <div class="col-lg-4">
-        <p style=" font-size: 24px; padding-top: 10px; text-align: center;"> เลขบัตรประชาชน 13 หลัก</p>
+<div id="bg-patient">
+    <br/><br/><br/>
+    <div class="row">
+        <div class="col-lg-4">
+            <p style=" font-size: 24px; padding-top: 10px; text-align: center;"> เลขบัตรประชาชน 13 หลัก</p>
+        </div>
+        <div class="col-lg-6">
+            <?php
+            $this->widget("ext.maskedInput.MaskedInput", array(
+                //"model" => $model,
+                //"attribute" => "card",
+                "id" => 'card',
+                "name" => 'card',
+                "mask" => '9-9999-99999-99-9',
+                "clientOptions" => array("autoUnmask" => true), /* autoUnmask defaults to false */
+                "defaults" => array("removeMaskOnSubmit" => false),
+                    /* once defaults are set will be applied to all the masked fields  removeMaskOnSubmit defaults to true */
+            ));
+            ?>
+        </div>
+        <div class="col-lg-2">
+            <button type="button" class="btn btn-primary btn-lg btn-block" onclick="Search()"><i class="fa fa-search"></i> ค้นหา</button>
+        </div>
     </div>
-    <div class="col-lg-6">
-        <?php
-        $this->widget("ext.maskedInput.MaskedInput", array(
-            //"model" => $model,
-            //"attribute" => "card",
-            "id" => 'card',
-            "name" => 'card',
-            "mask" => '9-9999-99999-99-9',
-            "clientOptions" => array("autoUnmask" => true), /* autoUnmask defaults to false */
-            "defaults" => array("removeMaskOnSubmit" => false),
-                /* once defaults are set will be applied to all the masked fields  removeMaskOnSubmit defaults to true */
-        ));
-        ?>
-    </div>
-    <div class="col-lg-2">
-        <button type="button" class="btn btn-primary btn-lg btn-block" onclick="Search()"><i class="fa fa-search"></i> ค้นหา</button>
-    </div>
+
+    <div id="patient_result"></div>
 </div>
-
-<div id="patient_result"></div>
-
 <script type="text/javascript">
     function Search() {
         var url = "<?php echo Yii::app()->createUrl('patient/dortorsearch') ?>";
         var card = $("#card").val();
         var data = {card: card};
-        
-        if(card == ""){
+
+        if (card == "") {
             $("#card").focus();
             return false;
         }
-        
+
         $.post(url, data, function (result) {
             if (result == 0) {
                 $("#patient_result").html("<hr/><p style='color:red;text-align:center; font-size:24px;'>ไม่พบข้อมูลลูกค้า</p>");
--- protected/views/patient/dortorsearch.php
+++ protected/views/patient/dortorsearch.php
@@ -1,22 +1,40 @@
 <?php
 $config = new Configweb_model();
-$type = $patient['type']
+$type = $patient['type'];
+
+if (!empty($patient['images'])) {
+    $img_profile = "uploads/profile/" . $patient['images'];
+} else {
+    if ($patient['sex'] == 'M') {
+        $img_profile = "images/Big-user-icon.png";
+    } else if ($patient['sex'] == 'F') {
+        $img_profile = "images/Big-user-icon-female.png";
+    } else {
+        $img_profile = "images/Big-user.png";
+    }
+}
 ?>
 <hr/>
 
-<table class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>ชื่อ - สกุล</th>
-            <th>อายุ</th>
-            <th>ประเภทลูกค้า</th>
-        </tr>
-    </thead>
+<table class="table table-bordered">
     <tbody>
         <tr>
-            <td><a href="<?php echo Yii::app()->createUrl('patient/view',array('id' => $patient['id']))?>"><?php echo $patient['name'] . ' ' . $patient['lname'] ?></a></td>
-            <td><?php echo $config->get_age($patient['birth']) ?></td>
-            <td><?php echo Gradcustomer::model()->find("id = '$type'")['grad']; ?></td>
+            <td style=" width: 20%;">
+                <img src="<?php echo Yii::app()->baseUrl; ?>/<?php echo $img_profile; ?>" class="img-responsive img-thumbnail" id="img_profile"/>
+            </td>
+            <td id="font-20">
+                ชื่อ - สกุล : 
+                <a href="<?php echo Yii::app()->createUrl('patient/view', array('id' => $patient['id'])) ?>">
+                    <?php echo Pername::model()->find("oid", $patient['oid'])['pername'] . $patient['name'] . ' ' . $patient['lname'] ?></a><br/>
+                อายุ: <?php echo $config->get_age($patient['birth']) ?> ปี<br/>
+                ประเภทลูกค้า: <?php echo Gradcustomer::model()->find("id = '$type'")['grad']; ?><br/>
+                อาชีพ : <?php echo Occupation::model()->find("id", $patient['occupation'])['occupationname']; ?><br/>
+                PID : <?php echo $patient['pid'] ?><br/>
+            </td>
         </tr>
     </tbody>
 </table>
+
+
+
+
